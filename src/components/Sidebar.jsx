95ccf0b6fdd1edfbd0375ce010d253fc8b6d0fd0
trishantjain <trishantjain8888@gmail.com>
2024-03-25 02:37:24 +0530

    Corrected Sidebar.jsx
    - Updated paranthesis of Icon Arrow function.
    - Removed unused comments

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,11 +2,9 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { logo, sun } from '../assets';
-// import Logo from '../assets/logo'
 import { navlinks } from '../constants';
-// import logo  from '../assets/logo.svg';
 
-const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => {
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
     <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
         {!isActive ? (
             <img src={imgUrl} alt='fund_logo' className='w-1/2 h-1/2' />
@@ -14,7 +12,7 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => {
             (<img src={imgUrl} alt='fund_logo' className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
             )}
     </div>
-}
+)
 
 const Sidebar = () => {
     const navigate = useNavigate();
@@ -24,9 +22,7 @@ const Sidebar = () => {
         <div>
             <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
                 <Link to="/">
-                    Before: Sidebar Comp
                     <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
-                    After: Sidebar Comp
                 </Link>
 
                 <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
